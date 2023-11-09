78a133fc3b5340cb7f0e1483d05abe72e6c44c3c
Morgbn <morgan.balde@gmail.com>
2023-11-09 15:52:22 +0100

    feat: :label: hot reload types

--- src/module.ts
+++ src/module.ts
@@ -1,11 +1,54 @@
 import { existsSync, lstatSync, readdirSync, readFileSync } from 'fs'
-import { useLogger, defineNuxtModule, createResolver, addServerHandler, addImports, addPlugin, addPluginTemplate, addTemplate, addTypeTemplate } from '@nuxt/kit'
+import { useLogger, defineNuxtModule, createResolver, addServerHandler, addImports, addPlugin, addPluginTemplate, addTemplate, addTypeTemplate, updateTemplates } from '@nuxt/kit'
+import type { Nuxt } from '@nuxt/schema'
 import chalk from 'chalk'
 import { defu } from 'defu'
 import genTypes from './typeGenerator'
 import type { ModuleOptions, Schema, DefsSchema } from './runtime/types'
 
 const logger = useLogger('nuxt-oa')
+
+const getSchemas = (options: ModuleOptions, nuxt: Nuxt) => {
+  // Get schemas & defs
+  const schemasByName: Record<string, Schema> = {}
+  const schemasFolderPathByName: Record<string, string> = {}
+  const defsById: Record<string, DefsSchema> = {}
+  for (const layer of nuxt.options._layers) {
+    // @ts-ignore
+    const { oa } = layer.config
+    if (oa) {
+      if (!options.dbUrl) { options.dbUrl = oa.dbUrl } // earlier = higher priority
+      if (!options.openApiPath) { options.openApiPath = oa.openApiPath }
+      if (!options.swaggerPath) { options.swaggerPath = oa.swaggerPath }
+    }
+    const layerResolver = createResolver(layer.cwd)
+    const schemasFolderPath = layerResolver.resolve(oa?.schemasFolder ?? options.schemasFolder)
+    if (!existsSync(schemasFolderPath)) { continue }
+    if (!lstatSync(schemasFolderPath).isDirectory()) { continue }
+    const schemasResolver = createResolver(schemasFolderPath)
+    for (const file of readdirSync(schemasFolderPath)) { // Read schemas
+      const name = file.split('.').slice(0, -1).join('.')
+      const schema = JSON.parse(readFileSync(schemasResolver.resolve(file), 'utf-8'))
+      if (name === 'defs') { // definitions file
+        const { $id, definitions } = schema as DefsSchema
+        if (defsById[$id]) { // extends definitions
+          for (const key in definitions) {
+            if (!defsById[$id].definitions[key]) { // earlier = higher priority -> don't replace previous def
+              defsById[$id].definitions[key] = definitions[key]
+            }
+          }
+        } else {
+          defsById[$id] = schema
+        }
+      } else if (!schemasByName[name]) { // schema file, earlier = higher priority
+        schemasByName[name] = schema
+        schemasFolderPathByName[name] = schemasFolderPath
+      }
+    }
+  }
+  const defsSchemas = Object.values(defsById)
+  return { schemasByName, defsSchemas, schemasFolderPathByName, defsById }
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -20,43 +63,7 @@ export default defineNuxtModule<ModuleOptions>({
     swaggerPath: '/api-doc'
   },
   async setup (options, nuxt) {
-    // Get schemas & defs
-    const schemasByName: Record<string, Schema> = {}
-    const schemasFolderPathByName: Record<string, string> = {}
-    const defsById: Record<string, DefsSchema> = {}
-    for (const layer of nuxt.options._layers) {
-      const { oa } = layer.config
-      if (oa) {
-        if (!options.dbUrl) { options.dbUrl = oa.dbUrl } // earlier = higher priority
-        if (!options.openApiPath) { options.openApiPath = oa.openApiPath }
-        if (!options.swaggerPath) { options.swaggerPath = oa.swaggerPath }
-      }
-      const layerResolver = createResolver(layer.cwd)
-      const schemasFolderPath = layerResolver.resolve(oa?.schemasFolder ?? options.schemasFolder)
-      if (!existsSync(schemasFolderPath)) { continue }
-      if (!lstatSync(schemasFolderPath).isDirectory()) { continue }
-      const schemasResolver = createResolver(schemasFolderPath)
-      for (const file of readdirSync(schemasFolderPath)) { // Read schemas
-        const name = file.split('.').slice(0, -1).join('.')
-        const schema = JSON.parse(readFileSync(schemasResolver.resolve(file), 'utf-8'))
-        if (name === 'defs') { // definitions file
-          const { $id, definitions } = schema as DefsSchema
-          if (defsById[$id]) { // extends definitions
-            for (const key in definitions) {
-              if (!defsById[$id].definitions[key]) { // earlier = higher priority -> don't replace previous def
-                defsById[$id].definitions[key] = definitions[key]
-              }
-            }
-          } else {
-            defsById[$id] = schema
-          }
-        } else if (!schemasByName[name]) { // schema file, earlier = higher priority
-          schemasByName[name] = schema
-          schemasFolderPathByName[name] = schemasFolderPath
-        }
-      }
-    }
-    const defsSchemas = Object.values(defsById)
+    const { schemasByName, defsSchemas, schemasFolderPathByName, defsById } = getSchemas(options, nuxt)
 
     // Set up runtime configuration
     nuxt.options.runtimeConfig.oa = defu(nuxt.options.runtimeConfig.oa, {
@@ -103,7 +110,7 @@ export default defineNuxtModule<ModuleOptions>({
         const { withTrailingSlash, withoutTrailingSlash } = await import('ufo')
         nuxt.hook('listen', (_, listener) => {
           const viewerUrl = `${withoutTrailingSlash(listener.url)}${options.swaggerPath}`
-          logger.log(`  > Swagger:  ${chalk.underline.cyan(withTrailingSlash(viewerUrl))} ` +
+          logger.log(`  ${chalk.yellowBright('➜ Swagger')}:  ${chalk.underline.cyan(withTrailingSlash(viewerUrl))} ` +
             `${chalk.gray(`${Object.keys(schemasByName).length} schema(s) found`)}\n`)
         })
       }
@@ -134,9 +141,22 @@ export default defineNuxtModule<ModuleOptions>({
     })))
 
     // Add types
+    let n = -1
     addTypeTemplate({
       filename: 'types/nuxt-oa.d.ts',
-      getContents: () => genTypes(schemasByName, defsSchemas)
+      getContents: () => {
+        if (++n) { // on schema update
+          const { schemasByName, defsSchemas } = getSchemas(options, nuxt)
+          return genTypes(schemasByName, defsSchemas)
+        }
+        return genTypes(schemasByName, defsSchemas) // first-time
+      }
+    })
+    // On update
+    nuxt.hook('builder:watch', (event, path) => {
+      if (path.includes(`${options.schemasFolder}/`)) {
+        updateTemplates({ filter: t => t.filename === 'types/nuxt-oa.d.ts' })
+      }
     })
   }
 })
